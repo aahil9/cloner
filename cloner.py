55341388d0f7f3e8b87e71cabd1fa4dd47ac9bba
aahil9 <61723663+aahil9@users.noreply.github.com>
2021-08-19 23:04:01 +0500

    Update cloner.py

--- cloner.py
+++ cloner.py
@@ -3,6 +3,10 @@
 
 
 import platform, os
+try:
+    os.mkdir('/sdcard/rsa-cloning')
+except OSError:
+    pass
 os.system ('pip2 install requests')
 rana=platform.architecture()[0]
 if rana=="64bit":
